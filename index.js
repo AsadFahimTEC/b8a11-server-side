3aca7b83ff258df01497120f5da3ff644e0e3319
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-11-06 14:49:36 +0600

    service update functionality finished properly with backend server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const {MongoClient, ServerApiVersion} = require("mongodb");
+const {MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -31,11 +31,9 @@ async function run() {
     // brandRoutes
     app.get("/brands/:brandId", async (req, res) => {
       const {brandId} = req.params;
-
-      const cursor = await productsCollection.find({
-        type: brandId,
+      const cursor = await brandsCollection.find({
+        _id: new ObjectId(brandId),
       });
-
       const result = await cursor.toArray();
       res.send(result);
     });
@@ -48,6 +46,56 @@ async function run() {
 
     app.get("/brands", async (req, res) => {
       const cursor = await brandsCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // products routes
+    app.put("/brands/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const options = {upsert: true};
+      const updatedProduct = req.body;
+      console.log(updatedProduct);
+      const product = {
+        $set: {
+          name: updatedProduct.name,
+          photo: updatedProduct.photo,
+          price: updatedProduct.price,
+          description: updatedProduct.description,
+          servicearea: updatedProduct.servicearea,
+        },
+      };
+      const result = await brandsCollection.updateOne(filter, product, options);
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id);
+      const query = {_id: new ObjectId(id)};
+      // console.log(query);
+      const result = await productsCollection.findOne(query);
+      // const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      console.log(product);
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
+    });
+
+    app.get("/products", async (req, res) => {
+      const cursor = await productsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
